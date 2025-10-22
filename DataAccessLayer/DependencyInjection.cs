e1611a377e3a6c4326e0d2de24a87800785f64f1
Farouk Umar Sedick <sadikalhanssah@gmail.com>
2025-10-22 15:59:13 +0000

    fix: connection string update with env variables incomplete

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -7,14 +7,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace DataAccessLayer;
 
-public static class DependencyInjection
+public static class  DependencyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
         string connectionStringTemplate = configuration.GetConnectionString("PostgresConnectionString")!;
 
         string connectionString = connectionStringTemplate
-            .Replace("$POSTGRES_HOST",Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+            .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
             .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
         
         // Add Data Access Layer services to IoC container
